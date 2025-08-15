e05f163cd480b1042d531eec754d760da0c694d5
Benjhamin <benjhamin.coca@ucb.edu.bo>
2025-08-15 10:58:18 -0400

    Add files via upload

--- Aprendiendo JS/Aprendiendo.js
+++ Aprendiendo JS/Aprendiendo.js
@@ -3,3 +3,22 @@ function mi_primer_algoritmo() {
     var mensaje = "¡Hola, mundo!";
     alert(mensaje);
 }
+// Con return
+function binarioADecimal(binario) {
+  let decimal = 0;
+  for (let i = 0; i < binario.length; i++) {
+    decimal = decimal * 2 + (binario[i] === '1' ? 1 : 0);
+  }
+  return decimal;
+}
+
+function sumarBinarios(bin1, bin2) {
+  const dec1 = binarioADecimal(bin1);
+  const dec2 = binarioADecimal(bin2);
+  return dec1 + dec2;
+}
+
+// Ejemplo
+let bin1 = "1010";
+let bin2 = "1101";
+console.log("Suma en decimal:", sumarBinarios(bin1, bin2));
